3279dc760db1a6c280d4cb974ff91951c384de62
kjm1550 <kjm1550@gmail.com>
2020-02-25 18:20:02 -0600

    Refactor Declarations

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -5,12 +5,9 @@ const hourHand = document.querySelector('.hour-hand');
 setTime = () => {
 	const now = new Date();
 
-	const seconds = now.getSeconds();
-	const secondsDegrees = (seconds / 60) * 360 + 90;
-	const mins = now.getMinutes();
-	const minsDegrees = (mins / 60) * 360 + 90;
-	const hours = now.getHours();
-	const hoursDegrees = (hours / 12) * 360 + 90;
+	const secondsDegrees = (now.getSeconds() / 60) * 360 + 90;
+	const minsDegrees = (now.getMinutes() / 60) * 360 + 90;
+	const hoursDegrees = (now.getHours() / 12) * 360 + 90;
 
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 	minHand.style.transform = `rotate(${minsDegrees}deg)`;
