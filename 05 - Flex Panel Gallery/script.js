ee18271b2f03a43ffa3566c2d21079d783288016
kjm1550 <kjm1550@gmail.com>
2020-02-27 21:10:48 -0600

    Flex Panal 05

---
+++ 05 - Flex Panel Gallery/script.js
@@ -0,0 +1,11 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+	this.classList.toggle('open');
+}
+function toggleActive(e) {
+	e.propertyName.includes('flex') ? this.classList.toggle('open-active') : 0;
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
