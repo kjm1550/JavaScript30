37055a4395755d66ff1ac8fc5039a716366e397d
kjm1550 <kjm1550@gmail.com>
2020-03-16 22:02:01 -0500

    Remove a Console Log

--- 20-Speech_Detection/script.js
+++ 20-Speech_Detection/script.js
@@ -8,7 +8,6 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-	console.log(e);
 	const transcript = Array.from(e.results)
 		.map(result => result[0])
 		.map(result => result.transcript)
